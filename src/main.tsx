eb8c59ae09d947bc02826d417f085ea77b1aa3b0
Salman <salman.876@outlook.com>
2024-10-11 15:13:30 +0300

    Setup emotion

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { App } from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import App from './App.tsx';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
---
+++ src/pages/portfolio/portfolio.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+import { Button } from './portfolio.styles';
+
+export const Portfolio: FC = () => {
+  return (
+    <>
+      <h1>This is a portfolio</h1>
+      <Button>Button</Button>
+    </>
+  );
+};
